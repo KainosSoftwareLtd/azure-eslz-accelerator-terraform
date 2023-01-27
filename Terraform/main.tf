70bfb91a39bb4fc65f68d3be1bb0a92d1fefd4b0
Rafal Pienczykowski <rafal.pienczykowski@kainos.com>
2023-01-27 16:33:21 +0100

    Deploy Custom Landing Zone Archetypes - example lvl. 200

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -12,10 +12,41 @@ module "enterprise_scale" {
   }
 
   root_parent_id = data.azurerm_client_config.core.tenant_id
-  root_id        = "alztf"
-  root_name      = "ALZ Terraform"
+  root_id        = var.root_id
+  root_name      = var.root_name
+  library_path   = "${path.root}/lib"
 
   #Deploy Demo Landing Zone Archetypes
   deploy_demo_landing_zones = true
+
+  custom_landing_zones = {
+    "${var.root_id}-online-example-1" = {
+      display_name               = "${upper(var.root_id)} Online Example 1"
+      parent_management_group_id = "${var.root_id}-landing-zones"
+      subscription_ids           = []
+      archetype_config = {
+        archetype_id   = "customer_online"
+        parameters     = {}
+        access_control = {}
+      }
+    }
+    "${var.root_id}-online-example-2" = {
+      display_name               = "${upper(var.root_id)} Online Example 2"
+      parent_management_group_id = "${var.root_id}-landing-zones"
+      subscription_ids           = []
+      archetype_config = {
+        archetype_id   = "customer_online"
+        parameters     = {
+          Deny-Resource-Locations = {
+            listOfAllowedLocations = ["westeurope",]
+          }
+          Deny-RSG-Locations = {
+            listOfAllowedLocations = ["westeurope",]
+          }
+        }
+        access_control = {}
+      }
+    }
+  }
 
 }
