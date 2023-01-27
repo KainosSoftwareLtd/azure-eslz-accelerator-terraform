661999cf5028c2aefe2379f8e9f9e2d754d25048
Rafal Pienczykowski <rafal.pienczykowski@kainos.com>
2023-01-27 15:36:47 +0100

    Deploy Default Configuration - example 100.

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,5 +1,18 @@
-#Create Resource Group
-resource "azurerm_resource_group" "alz-RG" {
-  name     = "alztf-rg"
-  location = "west Europe"
+# Call the caf-enterprise-scale module directly from the Terraform Registry
+# pinning to the latest version
+
+module "enterprise_scale" {
+  source  = "Azure/caf-enterprise-scale/azurerm"
+  version = "3.1.2"
+
+  providers = {
+    azurerm              = azurerm
+    azurerm.connectivity = azurerm
+    azurerm.management   = azurerm
+  }
+
+  root_parent_id = data.azurerm_client_config.core.tenant_id
+  root_id        = "alztf"
+  root_name      = "ALZ Terraform"
+
 }
