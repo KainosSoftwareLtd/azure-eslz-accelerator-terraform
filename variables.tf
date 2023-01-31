ed16837e55ce7da3e486ea39ba1399e4177df0f4
Rafal Pienczykowski <rafal.pienczykowski@kainos.com>
2023-01-31 15:07:22 +0100

    root_parent_id.

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@
 variable "root_parent_id" {
   type        = string
   description = "The root_parent_id is used to specify where to set the root for all Landing Zone deployments. Usually the Tenant ID when deploying the core Enterprise-scale Landing Zones."
-
+  default = "22590560-6a67-4e92-af77-78a0c45fe322"
   validation {
     condition     = can(regex("^[a-zA-Z0-9-_\\(\\)\\.]{1,36}$", var.root_parent_id))
     error_message = "Value must be a valid Management Group ID, consisting of alphanumeric characters, hyphens, underscores, periods and parentheses."
