40ba3481cd39336013e5699488d0d679ad8f28d6
Rafal Pienczykowski <rafal.pienczykowski@kainos.com>
2023-02-01 16:27:42 +0100

    deploying management components - resources location.

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -34,7 +34,7 @@ variable "security_alerts_email_address" {
 
 variable "management_resources_location" {
   type    = string
-  default = "uksouth"
+  default = "westeurope"
 }
 
 variable "management_resources_tags" {
