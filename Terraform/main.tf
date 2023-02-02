dc2a7d36caf2ed03e756bb2d7c3275520142d486
Rafal Pienczykowski <rafal.pienczykowski@kainos.com>
2023-02-02 13:24:19 +0100

    Adding comments to main.

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,5 +1,5 @@
 # Call the caf-enterprise-scale module directly from the Terraform Registry
-# pinning to the latest version
+# pinning to the selected version. Here "3.1.2"
 
 module "enterprise_scale" {
   source  = "Azure/caf-enterprise-scale/azurerm"
@@ -12,9 +12,14 @@ module "enterprise_scale" {
     azurerm.identity     = azurerm.identity
   }
 
+  # root_parent_id  - root for all Landing Zone deployments, Usually the Tenant ID
+  # root_id - a custom Name (ID) value for the Enterprise-scale "root" Management Group, and append this to the ID for all core Enterprise-scale Management Groups.
+  # root_name - a custom DisplayName value for the Enterprise-scale "root" Management Group.
   root_parent_id   = data.azurerm_client_config.core.tenant_id
   root_id          = var.root_id
   root_name        = var.root_name
+
+  // Default location for resources where specific location was not provided
   default_location = "westeurope"
 
   // Set subscription IDs for placement of platform subs
@@ -28,18 +33,18 @@ module "enterprise_scale" {
   // Deploy Demo Landing Zone Archetypes
   deploy_demo_landing_zones = true
 
-  // Deploy Custom Landing Zones
+  // Deploy Custom Landing Zones configured in locals.tf
   custom_landing_zones = local.custom_landing_zones
 
-  // Deploy Connectivity Resources
+  // Deploy Connectivity Resources with custom settings configured in locals.tf
   deploy_connectivity_resources    = var.deploy_connectivity_resources
   configure_connectivity_resources = local.configure_connectivity_resources
 
-  // Deploy Identity Resources
+  // Deploy Identity Resources with custom settings configured in locals.tf
   deploy_identity_resources    = var.deploy_identity_resources
   configure_identity_resources = local.configure_identity_resources
 
-  // Deploy Management Resources
+  // Deploy Management Resources with custom settings configured in locals.tf
   deploy_management_resources    = var.deploy_management_resources
   configure_management_resources = local.configure_management_resources
 }
