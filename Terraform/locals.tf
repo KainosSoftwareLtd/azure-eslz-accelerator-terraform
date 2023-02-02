5942c03900d88cfd06756430f6b547a51e1148b9
Rafal Pienczykowski <rafal.pienczykowski@kainos.com>
2023-02-02 12:54:13 +0100

    Moving custom_landing_zones to locals.

--- Terraform/locals.tf
+++ Terraform/locals.tf
@@ -1,6 +1,37 @@
 
 locals {
-  # Configure the management resources settings.
+  // Custom Landing Zones Archetypes
+  custom_landing_zones = {
+    "${var.root_id}-online-example-1" = {
+      display_name               = "${upper(var.root_id)} Online Example 1"
+      parent_management_group_id = "${var.root_id}-landing-zones"
+      subscription_ids           = []
+      archetype_config = {
+        archetype_id   = "customer_online"
+        parameters     = {}
+        access_control = {}
+      }
+    }
+    "${var.root_id}-online-example-2" = {
+      display_name               = "${upper(var.root_id)} Online Example 2"
+      parent_management_group_id = "${var.root_id}-landing-zones"
+      subscription_ids           = []
+      archetype_config = {
+        archetype_id = "customer_online"
+        parameters = {
+          Deny-Resource-Locations = {
+            listOfAllowedLocations = ["westeurope", ]
+          }
+          Deny-RSG-Locations = {
+            listOfAllowedLocations = ["westeurope", ]
+          }
+        }
+        access_control = {}
+      }
+    }
+  }
+
+  // Configure the management resources settings.
   configure_management_resources = {
     settings = {
       log_analytics = {
