75544d12f45c2c7548d5667eb1c4fd202d64588c
Ashley Rutland <ashley.rutland.kainos@bhsct.onmicrosoft.com>
2023-02-16 14:10:04 +0000

    clean up repo

--- Terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "alz-terraform"
-    storage_account_name = "alzterraformstatesalrs01"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
